6fc9a39c897cd658670bff6fdf80a173a7511480
George <1155096012@link.cuhk.edu.hk>
2019-06-04 10:29:10 +0800

    update a new controlcan library -> glh Jun. 4th 2019

--- sensor_startup/include/sensor_startup/controlcan.h
+++ sensor_startup/include/sensor_startup/controlcan.h
@@ -1,71 +1,15 @@
 #ifndef CONTROLCAN_H
 #define CONTROLCAN_H
 
+////文件版本：v2.01 20190527
 //接口卡类型定义
-#define VCI_PCI5121		1
-#define VCI_PCI9810		2
+
+#define VCI_USBCAN1		3
 #define VCI_USBCAN2		4
-#define VCI_PCI9820		5
-#define VCI_CAN232		6
-#define VCI_PCI5110		7
-#define VCI_CANLite		8
-#define VCI_ISA9620		9
-#define VCI_ISA5420		10
+#define VCI_USBCAN2A		4
 
-
-#define VCI_PC104CAN	11
-#define VCI_CANETUDP	12
-#define VCI_CANETE		12
-#define VCI_DNP9810		13
-#define VCI_PCI9840		14
-#define VCI_PC104CAN2	15
-#define VCI_PCI9820I	16
-#define VCI_CANETTCP	17
-#define VCI_PEC9920		18
-#define VCI_PCIE_9220	18
-#define VCI_PCI5010U	19
-#define VCI_USBCAN_2E_U 21
-#define VCI_PCI5020U	22
-#define VCI_EG20T_CAN	23
-#define VCI_PCIE9221	24
-#define VCI_WIFICAN_TCP 25
-#define VCI_WIFICAN_UDP 26
-#define VCI_PCIe9120	27
-#define VCI_PCIe9110	28
-#define VCI_PCIe9140	29
-
-/*
-  The following device ID is 
-  supported by this can analyst 
-*/
-#define VCI_USBCAN1         3
-#define VCI_USBCAN_E_U      20
-
-/*
-  Error code of CAN :
-
-  ERR_CAN_OVERFLOW --- internal FIFO overflow of can controller
-	ERR_CAN_ERRALARM --- error alarm of can controller
-  ERR_CAN_PASSIVE	 --- negative error of can controller
-	ERR_CAN_LOSE		 --- loss of arbitration
-	ERR_CAN_BUSERR	 --- can bus error
-*/
-#define	ERR_CAN_OVERFLOW			0x0001	
-#define	ERR_CAN_ERRALARM			0x0002	
-#define	ERR_CAN_PASSIVE				0x0004
-#define	ERR_CAN_LOSE				  0x0008
-#define	ERR_CAN_BUSERR				0x0010
-
-// general error code
-#define	ERR_DEVICEOPENED			0x0100	//设备已经打开
-#define	ERR_DEVICEOPEN				0x0200	//打开设备错误
-#define	ERR_DEVICENOTOPEN			0x0400	//设备没有打开
-#define	ERR_BUFFEROVERFLOW			0x0800	//缓冲区溢出
-#define	ERR_DEVICENOTEXIST			0x1000	//此设备不存在
-#define	ERR_LOADKERNELDLL			0x2000	//装载动态库失败
-#define ERR_CMDFAILED				0x4000	//执行命令失败错误码
-#define	ERR_BUFFERCREATE			0x8000	//内存不足
-
+#define VCI_USBCAN_E_U 		20
+#define VCI_USBCAN_2E_U 	21
 
 //函数调用返回状态值
 #define	STATUS_OK					1
@@ -76,7 +20,7 @@
 #define CHAR char
 #define UCHAR unsigned char
 #define UINT unsigned int
-#define DWORD unsigned int  
+#define DWORD unsigned int
 #define PVOID void*
 #define ULONG unsigned int
 #define INT int
@@ -85,7 +29,6 @@
 #define BOOL BYTE
 #define TRUE 1
 #define FALSE 0
-
 
 //1.ZLGCAN系列接口卡信息的数据类型。
 typedef  struct  _VCI_BOARD_INFO{
@@ -98,7 +41,7 @@ typedef  struct  _VCI_BOARD_INFO{
 		CHAR	str_Serial_Num[20];
 		CHAR	str_hw_Type[40];
 		USHORT	Reserved[4];
-} VCI_BOARD_INFO, *PVCI_BOARD_INFO; 
+} VCI_BOARD_INFO,*PVCI_BOARD_INFO; 
 
 //2.定义CAN信息帧的数据类型。
 typedef  struct  _VCI_CAN_OBJ{
@@ -111,29 +54,9 @@ typedef  struct  _VCI_CAN_OBJ{
 	BYTE	DataLen;
 	BYTE	Data[8];
 	BYTE	Reserved[3];
-}VCI_CAN_OBJ, *PVCI_CAN_OBJ;
+}VCI_CAN_OBJ,*PVCI_CAN_OBJ;
 
-//3.定义CAN控制器状态的数据类型。
-typedef struct _VCI_CAN_STATUS{
-	UCHAR	ErrInterrupt;
-	UCHAR	regMode;
-	UCHAR	regStatus;
-	UCHAR	regALCapture;
-	UCHAR	regECCapture; 
-	UCHAR	regEWLimit;
-	UCHAR	regRECounter; 
-	UCHAR	regTECounter;
-	DWORD	Reserved;
-}VCI_CAN_STATUS, *PVCI_CAN_STATUS;
-
-//4.定义错误信息的数据类型。
-typedef struct _ERR_INFO{
-		UINT	ErrCode;
-		BYTE	Passive_ErrData[3];
-		BYTE	ArLost_ErrData;
-} VCI_ERR_INFO, *PVCI_ERR_INFO;
-
-//5.定义初始化CAN的数据类型
+//3.定义初始化CAN的数据类型
 typedef struct _INIT_CONFIG{
 	DWORD	AccCode;
 	DWORD	AccMask;
@@ -142,7 +65,14 @@ typedef struct _INIT_CONFIG{
 	UCHAR	Timing0;	
 	UCHAR	Timing1;	
 	UCHAR	Mode;
-}VCI_INIT_CONFIG, *PVCI_INIT_CONFIG;
+}VCI_INIT_CONFIG,*PVCI_INIT_CONFIG;
+
+///////// new add struct for filter /////////
+typedef struct _VCI_FILTER_RECORD{
+	DWORD ExtFrame;	//是否为扩展帧
+	DWORD Start;
+	DWORD End;
+}VCI_FILTER_RECORD,*PVCI_FILTER_RECORD;
 
 #ifdef __cplusplus
 #define EXTERN_C  extern "C"
@@ -150,32 +80,25 @@ typedef struct _INIT_CONFIG{
 #define EXTERN_C
 #endif
 
-EXTERN_C DWORD VCI_OpenDevice(DWORD DeviceType, DWORD DeviceInd, DWORD Reserved);
-EXTERN_C DWORD VCI_CloseDevice(DWORD DeviceType, DWORD DeviceInd);
-EXTERN_C DWORD VCI_InitCAN(DWORD DeviceType,  DWORD DeviceInd, 
-                           DWORD CANInd, PVCI_INIT_CONFIG pInitConfig);
+EXTERN_C DWORD VCI_OpenDevice(DWORD DeviceType,DWORD DeviceInd,DWORD Reserved);
+EXTERN_C DWORD VCI_CloseDevice(DWORD DeviceType,DWORD DeviceInd);
+EXTERN_C DWORD VCI_InitCAN(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd, PVCI_INIT_CONFIG pInitConfig);
 
-EXTERN_C DWORD VCI_ReadBoardInfo(DWORD DeviceType, DWORD DeviceInd, PVCI_BOARD_INFO pInfo);
-EXTERN_C DWORD VCI_ReadErrInfo(DWORD DeviceType, DWORD DeviceInd, 
-                              DWORD CANInd, PVCI_ERR_INFO pErrInfo);
-EXTERN_C DWORD VCI_ReadCANStatus(DWORD DeviceType, DWORD DeviceInd, 
-                                 DWORD CANInd, PVCI_CAN_STATUS pCANStatus);
+EXTERN_C DWORD VCI_ReadBoardInfo(DWORD DeviceType,DWORD DeviceInd,PVCI_BOARD_INFO pInfo);
 
-EXTERN_C DWORD VCI_GetReference(DWORD DeviceType, DWORD DeviceInd, 
-                                DWORD CANInd, DWORD RefType, PVOID pData);
-EXTERN_C DWORD VCI_SetReference(DWORD DeviceType, DWORD DeviceInd, 
-                                DWORD CANInd, DWORD RefType, PVOID pData);
+EXTERN_C DWORD VCI_SetReference(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd,DWORD RefType,PVOID pData);
 
-EXTERN_C ULONG VCI_GetReceiveNum(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd);
-EXTERN_C DWORD VCI_ClearBuffer(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd);
+EXTERN_C ULONG VCI_GetReceiveNum(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd);
+EXTERN_C DWORD VCI_ClearBuffer(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd);
 
-EXTERN_C DWORD VCI_StartCAN(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd);
-EXTERN_C DWORD VCI_ResetCAN(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd);
+EXTERN_C DWORD VCI_StartCAN(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd);
+EXTERN_C DWORD VCI_ResetCAN(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd);
 
-EXTERN_C ULONG VCI_Transmit(DWORD DeviceType, DWORD DeviceInd, 
-                            DWORD CANInd, PVCI_CAN_OBJ pSend, UINT Len);
-EXTERN_C ULONG VCI_Receive(DWORD DeviceType, DWORD DeviceInd, DWORD CANInd, 
-                           PVCI_CAN_OBJ pReceive, UINT Len, INT WaitTime);
+EXTERN_C ULONG VCI_Transmit(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd,PVCI_CAN_OBJ pSend,UINT Len);
+EXTERN_C ULONG VCI_Receive(DWORD DeviceType,DWORD DeviceInd,DWORD CANInd,PVCI_CAN_OBJ pReceive,UINT Len,INT WaitTime);
+
+EXTERN_C DWORD  VCI_UsbDeviceReset(DWORD DevType,DWORD DevIndex,DWORD Reserved);
+EXTERN_C DWORD  VCI_FindUsbDevice2(PVCI_BOARD_INFO pInfo);
 
 #endif
 
