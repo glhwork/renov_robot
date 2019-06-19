824fa43617ba282eaeda15244360461ae5c24747
George <1155096012@link.cuhk.edu.hk>
2019-06-19 10:44:35 +0800

    modify abs_encoder node to set address and read four encoders in the meantime -> glh Jun. 19th 2019

--- sensor_startup/src/test/abs_encoder.cpp
+++ sensor_startup/src/test/abs_encoder.cpp
@@ -1,17 +1,47 @@
 #include <iostream>
 #include <serial/serial.h>
 #include <string>
+#include <unistd.h>
 
 #include "ros/ros.h"
 #include "sensor_msgs/Range.h"
 
+#define ENCODER_1 0x01
+#define ENCODER_2 0x02
+#define ENCODER_3 0x03
+#define ENCODER_4 0x04
+
 typedef uint8_t _u8;
 typedef uint16_t _u16;
 _u8 cmd[8] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xc4, 0x0b};
+_u8 SET_ADDRESS_1[8] = {0x01, 0x06, 0x00, 0x04, 0x00, 0x01, 0x09, 0xcb};
+_u8 SET_ADDRESS_2[8] = {0x01, 0x06, 0x00, 0x04, 0x00, 0x02, 0x49, 0xca};
+_u8 SET_ADDRESS_3[8] = {0x01, 0x06, 0x00, 0x04, 0x00, 0x03, 0x88, 0x0a};
+_u8 SET_ADDRESS_4[8] = {0x01, 0x06, 0x00, 0x04, 0x00, 0x04, 0xc9, 0xc8};
+
+_u8 REQUEST_DATA_1[8] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0xc4, 0x0b};
+_u8 REQUEST_DATA_2[8] = {0x02, 0x03, 0x00, 0x00, 0x00, 0x02, 0xc4, 0x38};
+_u8 REQUEST_DATA_3[8] = {0x03, 0x03, 0x00, 0x00, 0x00, 0x02, 0xc5, 0xe9};
+_u8 REQUEST_DATA_4[8] = {0x04, 0x03, 0x00, 0x00, 0x00, 0x02, 0xc4, 0x5e};
+
+/* set the address
+ 01 06 00 04 00 01 09 cb
+ 01 06 00 04 00 02 49 ca
+ 01 06 00 04 00 03 88 0a
+ 01 06 00 04 00 04 c9 c8
+ */
+
+/* data request
+ 01 03 00 00 00 02 c4 0b
+ 02 03 00 00 00 02 c4 38
+ 03 03 00 00 00 02 c5 e9
+ 04 03 00 00 00 02 c4 5e
+ */
 serial::Serial encod_ser;
 
 void InitSerial(const std::string& port_id, const int& baudrate);
-void Loop(ros::Publisher pub);
+void SetAddress();
+void ReadEncoder();
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "abs_encoder");
@@ -31,8 +61,9 @@ int main(int argc, char** argv) {
 
   InitSerial(port, baudrate);
   if (encod_ser.isOpen()) {
+    SetAddress();
     while(ros::ok()) {
-      Loop(pub);
+      ReadEncoder();
       ros::spinOnce();
       r.sleep();
     }
@@ -58,32 +89,58 @@ void InitSerial(const std::string& port_id, const int& baudrate) {
 
 }
 
-void Loop(ros::Publisher pub) {
-  size_t n = sizeof(cmd) / sizeof(cmd[0]);
-  encod_ser.write(cmd, n);
+void SetAddress() {
 
-  if (encod_ser.available()) {
-    std::vector<_u8> data;
-    int size = encod_ser.available();
-    encod_ser.read(data, size); 
-    
-    for (size_t i = 0; i < size; i++) {
-      std::cout << "0x" << std::hex << (_u16)data[i] << "  ";
-    } 
-    std::cout << std::endl;
+  encod_ser.write(SET_ADDRESS_1, 8);
+  usleep(100);
+  encod_ser.write(SET_ADDRESS_2, 8);
+  usleep(100);
+  encod_ser.write(SET_ADDRESS_3, 8);
+  usleep(100);
+  encod_ser.write(SET_ADDRESS_4, 8);
+  usleep(100);
 
-    int result;
-    result = ((_u16)data[3] << 24) + 
-             ((_u16)data[4] << 16) +
-             ((_u16)data[5] << 8) + 
-             (_u16)data[6]; 
-
-    sensor_msgs::Range impulse;
-    impulse.header.frame_id = "abs_encoder";
-    impulse.header.stamp = ros::Time::now();
-
-    impulse.range = (double)result;
-    pub.publish(impulse);
-  }
-  
+  ROS_INFO("set address finish");
 }
+
+void ReadEncoder() {
+  std::vector<_u8> data_1, data_2, data_3, data_4;
+
+  encod_ser.write(REQUEST_DATA_1, 8);
+  if (encod_ser.available()) {
+    int size = encod_ser.available();
+    encod_ser.read(data_1, size);
+  }
+  int result_1;
+  result_1 = ((_u16)data_1[3] << 24) + ((_u16)data_1[4] << 16) +
+             ((_u16)data_1[5] << 8) + (_u16)data_1[6];
+
+  encod_ser.write(REQUEST_DATA_2, 8);
+  if (encod_ser.available()) {
+    int size = encod_ser.available();
+    encod_ser.read(data_2, size);
+  }
+  int result_2;
+  result_2 = ((_u16)data_2[3] << 24) + ((_u16)data_2[4] << 16) +
+             ((_u16)data_2[5] << 8) + (_u16)data_2[6];
+
+  encod_ser.write(REQUEST_DATA_3, 8);
+  if (encod_ser.available()) {
+    int size = encod_ser.available();
+    encod_ser.read(data_3, size);
+  }
+  int result_3;
+  result_3 = ((_u16)data_3[3] << 24) + ((_u16)data_3[4] << 16) +
+             ((_u16)data_3[5] << 8) + (_u16)data_3[6];
+
+  encod_ser.write(REQUEST_DATA_4, 8);
+  if (encod_ser.available()) {
+    int size = encod_ser.available();
+    encod_ser.read(data_4, size);
+  }
+  int result_4;
+  result_4 = ((_u16)data_4[3] << 24) + ((_u16)data_4[4] << 16) +
+             ((_u16)data_4[5] << 8) + (_u16)data_4[6];
+}
+
+
