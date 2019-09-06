13c5265c0e83a5655670959a9b9042bd710ff37e
George <1155096012@link.cuhk.edu.hk>
2019-09-06 16:39:21 +0800

    add basic functions to can_application

--- sensor_startup/include/sensor_startup/kinco/can_application.h
+++ sensor_startup/include/sensor_startup/kinco/can_application.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <yaml-cpp/yaml.h>
 
 #include "sensor_startup/controlcan.h"
 
@@ -14,8 +15,19 @@ class CanApplication {
  public:
   CanApplication();
   virtual ~CanApplication();
+  // read parameters in configuration files which is used to initialize can device
+  virtual void ReadFile(const std::string& file_address);
+  // open, initialize and start the can device
+  void CanActivate(const std::string& file_address);
+  PVCI_CAN_OBJ GetVciObject(const int& obj_num, const uint& initial_id);
+  void SendCommand(PVCI_CAN_OBJ obj, const uint& len);
 
  private:
+  void CanDiagnostic(const std::string& description, const int& state);
+  int device_type_;
+  int device_index_;
+  int can_index_;
+  int frame_len_;
 };  // class CanApplication
 
 }  // namespace mobile_base
